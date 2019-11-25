ad46ce9fba6e31d1ab68cf6d73b84c911a6f8cea
mdb1710 <mdb1710@gmail.com>
2019-11-24 23:19:57 -0500

    changed config

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
+  siteTitle: "90s Teen Culture - A Time Machine Back To The Greatest Decade", // <title>
+  shortSiteTitle: "90s Teen Culture", // <title> ending for posts and pages
+  siteDescription: "A blog about being a teen in the 90s",
   siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
   // pathPrefix: "",
   siteImage: "preview.jpg",
@@ -12,8 +12,8 @@ module.exports = {
   authorTwitterAccount: "greglobinski",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "90's Teen Culture",
+  headerSubTitle: "A Time Machine Back To The Greatest Decade",
 
   /* manifest.json */
   manifestName: "HeroBlog - a GatsbyJS starter",
